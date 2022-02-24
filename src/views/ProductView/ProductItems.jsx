efc38ea909766daf4958c1b74c9d108ec65956ad
Nattarintra Thutsanti <nattarintra@headofsearch.se>
2022-02-24 21:15:32 +0100

    Fix each must have key unique.

--- src/views/ProductView/ProductItems.jsx
+++ src/views/ProductView/ProductItems.jsx
@@ -2,12 +2,12 @@ import React from "react"
 import ProductCard from "@components/product-card/ProductCard"
 import data from "@data/products"
 
-const ProductItems = () => {
+const ProductItems = ({ cartItemQty }) => {
   return (
     <div className="grid-wrapper">
       {data.map(product => (
-        <div className="grid">
-          <ProductCard key={product.id} title={product.title} image={product.image} prices={product.prices} />
+        <div id={product.id} className="grid" key={product.id}>
+          <ProductCard title={product.title} image={product.image} prices={product.prices} />
         </div>
       ))}
     </div>
